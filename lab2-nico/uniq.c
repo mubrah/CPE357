c16ad8a09bd03190a433462b2479cc442c8e9b6e
Justin Nguyen <justinnuwin@gmail.com>
2018-01-26 13:31:22 +0000

    Finish lab2-nico

--- lab2-nico/uniq.c
+++ lab2-nico/uniq.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void read_long_line(FILE *file) {
+    int bufferLength = 256;
+    int bufferIdx = 0;
+    char *stringBuffer = (char *)malloc(bufferLength * sizeof(*stringBuffer));
+    char *previousLine = (char *)calloc(1, sizeof(*previousLine));
 
-char *read_long_line(FILE *file) {
-    
-    
+    while (1) {
+        char c = fgetc(file);
+        if (feof(file)) {
+            break;
+        } else {
+            if (bufferIdx == bufferLength) {
+                bufferLength *= 2;
+                stringBuffer = (char *)realloc(stringBuffer - bufferIdx, bufferLength);
+                stringBuffer += bufferIdx;
+            }
+            if (c == '\n') {
+                *stringBuffer = '\0';
+                stringBuffer -= bufferIdx;
+                if (strcmp(stringBuffer, previousLine)) {
+                    printf("%s\n", stringBuffer);
+                }
+                previousLine = (char *)realloc(previousLine,
+                                           bufferIdx+1 * sizeof(*previousLine));
+                strcpy(previousLine, stringBuffer);
+                bufferIdx = 0;
+            } else {
+                *stringBuffer = c;
+                stringBuffer++;
+                bufferIdx++;
+            }
+        }
+    }
+    free(stringBuffer);
+    free(previousLine);
 }
 
-int main(void) {
-    buffer
+int main(int argc, char **argv) {
+    
+    FILE *file = fopen(argv[1], "r");
+    read_long_line(file); 
+    return 0;
+}   
